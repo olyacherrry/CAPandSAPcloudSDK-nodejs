878e8fe3fd39831e13a89b87286b7896b3925c85
Volha Berastneva <olga.berestneva23@gmail.com>
2021-05-27 07:53:47 +0000

    add odata v2 proxy

--- srv/address-manager-service.js
+++ srv/address-manager-service.js
@@ -22,10 +22,23 @@ module.exports = cds.service.impl(async function () {
             //columns which we have declared in cds entity that we want to expose
             let columnsToSelect = ["BusinessPartner", "FirstName", "LastName"];
 
-            return await tx.run(
-                SELECT.from(entity)
-                    .columns(columnsToSelect)
-            )
+            //if there is a parameter
+            if (req.params[0]) {
+                //If you look in the .csn file you will see that
+                //the key for our BusinessPartner entity is BusinessPartner column
+                const businessPartner = req.params[0].BusinessPartner;
+                return await tx.run(
+                    SELECT.from(entity)
+                        .columns(columnsToSelect)
+                        .where({ 'BusinessPartner': businessPartner })
+                )
+            } else {
+                //if no parameter, we read all Business Partners
+                return await tx.run(
+                    SELECT.from(entity)
+                        .columns(columnsToSelect)
+                )
+            }
 
         } catch (err) {
             req.reject(err);
@@ -43,10 +56,28 @@ module.exports = cds.service.impl(async function () {
             //columns which we have declared in cds entity that we want to expose
             let columnsToSelect = ["BusinessPartner", "AddressID", "Country", "PostalCode", "CityName", "StreetName", "HouseNumber"];
 
-            return await tx.run(
-                SELECT.from(entity)
-                    .columns(columnsToSelect)
-            )
+            //if there is parameter
+            if (req.params[0]) {
+                //If you look in the .csn file you will see that
+                //the keys for our BusinessPartnerAddress entity are
+                //BusinessPartner and AddressID columns
+                const businessPartner = req.params[0].BusinessPartner;
+                const addressId = req.params[0].AddressID;
+                return await tx.run(
+                    SELECT.from(entity)
+                        .columns(columnsToSelect)
+                        .where({
+                            'BusinessPartner': businessPartner,
+                            'AddressID': addressId
+                        })
+                )
+            } else {
+                //if no parameter, we read all Business Partner Addresses
+                return await tx.run(
+                    SELECT.from(entity)
+                        .columns(columnsToSelect)
+                )
+            }
 
         } catch (err) {
             req.reject(err);
